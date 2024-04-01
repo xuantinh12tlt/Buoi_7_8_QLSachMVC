0e951a9ec3a996ff67e7d25f029b725415b3386e
xuantinh12tlt <xuantinh12tlt@gmail.com>
2024-04-01 19:55:45 +0700

    commit

---
+++ CommonLibs/DataValidity.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibs
+{
+    public class DataValidity
+    {
+        //Check if the data is numeric
+        public static bool CheckIsNumber(string s)
+        {
+            int a;
+            if (int.TryParse(s, out a))
+            {
+                return true;
+            }
+            return false;
+        }
+        /// <summary>
+        /// Check if the data is an integer
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static bool CheckIsNumberInt(string s)
+        {
+            int a;
+            if (int.TryParse(s, out a))
+            {
+                if (int.MaxValue >= a)
+                    return true;
+            }
+            return false;
+        }
+        /// <summary>
+        /// Check if the date is correct
+        /// </summary>
+        /// <param name="sDate"></param>
+        /// <returns></returns>
+        public static bool CheckIsDateTime(string sDate)
+        {
+            DateTime dDatetime;
+            if (DateTime.TryParse(sDate, out dDatetime))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ CommonLibs/Sercurity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibs
+{
+    public class Sercurity
+    {
+        public static bool HasXssFilterChars(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return false;
+            s = s.ToLower();
+            return s.Contains("<") || s.Contains(">") ||
+                   s.Contains("%3c") || s.Contains("%3e") ||
+                   s.Contains("＜") || s.Contains("＞") ||
+                   s.Contains("%ef%bc%9c") || s.Contains("%ef%bc%9e");
+        }
+    }
+}
